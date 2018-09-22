4f9b1b6d92bb3a0e9b608de45d17e2d1383d2e93
SergeyPunko <punkosergey1997@gmail.com>
2018-09-23 01:50:53 +0300

    done

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ module.exports = function getZerosCount(number, base) {
         var a = 0;
         while(base % i == 0){
             a++;
-            base = Math.trunc(base / i);
+            base = ~~(base / i);
         }
         var x = 0;
         var  num2 = number;
         while (num2 / i > 0){
-            x = x + Math.trunc(num2 / i);
-            num2 = Math.trunc(num2 / i);
+            x = x + (~~(num2 / i));
+            num2 = ~~(num2 / i);
         }
         if (num > x / a){
             num = x / a;
         }
 
     }
-    return Math.trunc(num);
+    return ~~(num);
 };
